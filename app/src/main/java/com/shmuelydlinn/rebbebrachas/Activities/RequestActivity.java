8888df5811e94ece57b8efc7307701a87a02dc8e
samdlinn <shmueld770@hotmail.com>
2016-05-31 18:45:32 +0300

    final touches before 1.1 release touched up request screen

--- app/src/main/java/com/shmuelydlinn/rebbebrachas/Activities/RequestActivity.java
+++ app/src/main/java/com/shmuelydlinn/rebbebrachas/Activities/RequestActivity.java
@@ -7,14 +7,12 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.ButtonBarLayout;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.shmuelydlinn.rebbebrachas.R;
