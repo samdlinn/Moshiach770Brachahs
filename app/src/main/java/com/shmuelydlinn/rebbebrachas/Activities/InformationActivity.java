354ca08a26cf9094d2d8a83174846eb3c1f9b0e8
samdlinn <shmueld770@hotmail.com>
2016-05-31 14:15:31 +0300

    Added paragraph formating and URL clickablilty to links

--- app/src/main/java/com/shmuelydlinn/rebbebrachas/Activities/InformationActivity.java
+++ app/src/main/java/com/shmuelydlinn/rebbebrachas/Activities/InformationActivity.java
@@ -3,6 +3,8 @@ package com.shmuelydlinn.rebbebrachas.Activities;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 import com.shmuelydlinn.rebbebrachas.R;
 
@@ -16,5 +18,12 @@ public class InformationActivity extends AppCompatActivity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.information_activity);
+
+        TextView text = (TextView) findViewById(R.id.information_text);
+        if (text != null) {
+//            text.setText(Html.fromHtml(getString(R.string.instructions_text)))/;
+            text.setMovementMethod(LinkMovementMethod.getInstance());
+//            Linkify.addLinks(text, Linkify.ALL);
+        }
     }
 }
--- app/src/main/java/com/shmuelydlinn/rebbebrachas/Activities/InstructionActivity.java
+++ app/src/main/java/com/shmuelydlinn/rebbebrachas/Activities/InstructionActivity.java
@@ -3,12 +3,16 @@ package com.shmuelydlinn.rebbebrachas.Activities;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
+import android.widget.TextView;
 
 import com.shmuelydlinn.rebbebrachas.R;
 
 /**
  * Created by shmueldlinn on 5/30/16.
- * This Activity handlers the instuction frame
+ * This Activity handlers the instruction frame
  */
 
 public class InstructionActivity extends AppCompatActivity {
@@ -16,6 +20,5 @@ public class InstructionActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.instruction_activity);
-
     }
 }
